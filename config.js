99cd298aa3572783a02b75bd6716c73b7eff9eee
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-01-16 01:58:09 +0300

    added bot prefix

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const config = {
-    TOKEN: "" //Write your discord bot token.
+    TOKEN: "", //Write your discord bot token.
+    prefix: "" //write your discord bot prefix
 }
 module.exports = config
